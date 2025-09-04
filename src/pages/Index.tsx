f14750b7f1dfa3a430224e63498f8ba38283ade1
hasib6464 <hasibmahmud6464@gmail.com>
2025-09-04 11:57:37 +0600

    hbghub

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,34 +1,30 @@
 import { Navigation } from '@/components/Navigation'
 import { HeroSection } from '@/components/HeroSection'
 import { ProductSpotlight, onyxProduct, pulseProduct } from '@/components/ProductSpotlight'
-import { FeaturesSection } from '@/components/FeaturesSection'
-import { AboutSection } from '@/components/AboutSection'
-import { TestimonialsSection } from '@/components/TestimonialsSection'
-import { CTASection } from '@/components/CTASection'
-import { Footer } from '@/components/Footer'
+import { FeaturesSection } from "@/components/FeaturesSection";
+import { TestimonialsSection } from "@/components/TestimonialsSection";
+import { CTASection } from "@/components/CTASection";
+import { Footer } from "@/components/Footer";
 
 const Index = () => {
   return (
     <div className="min-h-screen bg-background">
       <Navigation />
-      
+
       <main>
         <HeroSection />
-        
         <section id="products" className="relative">
           <ProductSpotlight product={onyxProduct} />
           <ProductSpotlight product={pulseProduct} />
         </section>
-        
         <FeaturesSection />
-        <AboutSection />
         <TestimonialsSection />
         <CTASection />
       </main>
-      
+
       <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default Index
